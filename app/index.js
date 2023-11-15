a0cb08e52f6df537d90feaa398d597817a92a555
Daniel <sotodaniel531@gmail.com>
2023-11-14 16:12:23 -0800

    added js code that installs middleware and sets up server

--- app/index.js
+++ app/index.js
@@ -1,21 +1,33 @@
 const express =  require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const server = require("http").Server(app);
 const port = 3000;
+const cors = require("cors");
+const helmet = require("helmet")
+
 const db = require("./queries");
 
+app.use(helmet());
 app.use(bodyParser.json());
-app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
-);
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
 
 app.get("/teams", db.getTeams);
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}.`);
+
+//pre-flight requests
+app.options('*', function(req, res) {
+	res.send(200);
+});
+
+server.listen(port, (err) => {
+    if (err){
+        throw err;
+    }
+
+    console.log(`Server running on port ${port}`)
 });
 
 
--- app/queries.js
+++ app/queries.js
@@ -14,6 +14,16 @@ const getTeams = (request, response) => {
 }
 
 
+/*
+ENDPOINTS 
+
+- Search Player By Name (give N closest names) Name Team
+- Display Statistics based on team name search return all 3 seasons.
+- Display Match Results based on team name return all 3 seasons
+
+*/
+
+
 module.exports = {
     getTeams
 };
